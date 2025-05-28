f553cd5a3f0068fb48265a404f206f340a28ada3
ChrispyBacon-dev <ori@hotmail.de>
2025-05-28 13:47:43 +0200

    ✨v1.8.4

--- dockflare/app/main.py
+++ dockflare/app/main.py
@@ -179,7 +179,7 @@ def main_application_entrypoint():
 
     logging.info("-" * 52)
     logging.info("--- DockFlare Starting ---")
-    logging.info(f"--- Version: 1.8.0 ---") 
+    logging.info(f"--- Version: 1.8.4 ---") 
     logging.info("-" * 52)
 
     load_state() 
