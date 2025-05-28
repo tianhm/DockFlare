42a24351114c4ce7a7fc34c487a6d3c5d12e0f1d
ChrispyBacon-dev <ori@hotmail.de>
2025-05-28 12:58:26 +0200

    tailwind:css savelist update, rebuild css

--- dockflare/tailwind.config.js
+++ dockflare/tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
   plugins: [
     require("daisyui"),
   ],
+   safelist: [
+    'h-3',
+    'w-3',
+    'inline-block',
+    'mr-1',
+    'text-info',
+  ],
   daisyui: {
       themes: [ 
       "light",
