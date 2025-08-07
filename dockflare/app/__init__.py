4c493f43aaad1c90f88e2d1ed67f21a0f908f6f3
ChrispyBacon-dev <ori@hotmail.de>
2025-08-07 08:24:40 +0200

    .

--- dockflare/app/__init__.py
+++ dockflare/app/__init__.py
@@ -12,8 +12,8 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# 
 # app/__init__.py
 import logging
 import queue
